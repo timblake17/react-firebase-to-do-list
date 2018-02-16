8eeb234700736454226180457a9a16ba9cb3e154
Tim Blake <timblake17x@gmail.com>
2018-02-15 21:21:34 -0500

    config added

---
+++ src/Config/config.js
@@ -0,0 +1,8 @@
+export const  DB_CONFIG =  {
+  
+    authDomain: "react-to-do-redone.firebaseapp.com",
+    databaseURL: "https://react-to-do-redone.firebaseio.com",
+    projectId: "react-to-do-redone",
+    storageBucket: "react-to-do-redone.appspot.com",
+    messagingSenderId: "624534892673"
+  };
